913bc971888476c010299494575e71c4b63be352
MohitGoel109 <goel0277@gmail.com>
2026-06-24 13:15:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(5000, () => {
+app.listen(5100, () => {
 
-  console.log("Server Running on Port 5000");
+  console.log("Server Running on Port 5100");
 
 });
